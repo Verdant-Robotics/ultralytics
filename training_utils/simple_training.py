927249dcfa66c456bcf847421ea4f1e930757bd0
sjhpark <sampark.jh@gmail.com>
2025-08-06 22:50:56 +0000

    make epochs and patience optional arguments

--- training_utils/simple_training.py
+++ training_utils/simple_training.py
@@ -19,9 +19,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train the model")
     parser.add_argument("-l", "--load", type=str, default=None, help="Path to the model weights to load. Load the pretrained model")
     parser.add_argument("-r", "--learning-rate", type=float, default=0.01, help="Learning rate for training")
+    parser.add_argument("-e", "--epochs", type=int, default=200, help="Number of epochs for training")
+    parser.add_argument("-p", "--patience", type=int, default=50, help="Number of epochs triggering early stopping when no improvement")
     parser.add_argument("-d", "--disable-wandb", action="store_true", help="Disable wandb logging")
 
     args = parser.parse_args()
+
+    print(f"Model weights initialized from: {args.load if args.load else 'scratch'}")
+    print(f"Learning rate: {args.learning_rate}, Epochs: {args.epochs}, Patience: {args.patience}")
 
     PrepareDataset(coco_classes_file, dataset_yaml_path, training_task)
 
@@ -43,7 +48,7 @@ if __name__ == "__main__":
         optimizer='SGD',
         lr0=args.learning_rate,
         lrf=0.01,
-        epochs=300,
+        epochs=args.epochs,
         flipud=0.5,
         fliplr=0.5,
         scale=0.2,
@@ -55,7 +60,7 @@ if __name__ == "__main__":
         batch=128,
         name=experiment_name,
         device=[0, 1, 2, 3, 4, 5, 6, 7],
-        patience=50,
+        patience=args.patience,
     )
 
     latest_weights_dir = GetLatestWeightsDir()
