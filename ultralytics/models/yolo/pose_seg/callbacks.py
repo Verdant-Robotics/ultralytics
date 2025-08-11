359118e6b45a581cae9736ed4eeaa6acf3e605ec
Ava <ava.abderezaei@gmail.com>
2025-08-11 17:48:09 +0000

    Remove comments

--- ultralytics/models/yolo/pose_seg/callbacks.py
+++ ultralytics/models/yolo/pose_seg/callbacks.py
@@ -1,16 +1,19 @@
 
 def on_train_batch_start(trainer):
-    print('on train batch start')
+    # print('on train batch start')
     # breakpoint()
+    pass
 
 
 def on_train_batch_end(trainer):
-    print('on train batch end')
+    # print('on train batch end')
     # trainer.train_loader.update_dataset()
     # breakpoint()
+    pass
 
 
 
 def on_train_epoch_end(trainer):
-    print('on train epoch end')
+    # print('on train epoch end')
     # breakpoint()
+    pass
